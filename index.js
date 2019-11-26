f59af4fa129c0872c5cc2b82e57ec73c636a3ba7
HirudineeADRT <hirudinee@adroitlogic.com>
2019-11-26 12:02:56 +0530

    added python

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.hiru1126gcppythonfunction = require("./hiru1126gcppython/function.js").handler;
